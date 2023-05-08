9316583963b12c5e0168820eb6c20207ac1f1b3d
elenarojano <elenarojano@uma.es>
2023-05-08 14:05:56 +0200

    Changes to make ASP calculation and hub filter increased from 2.5 to 3.

--- executions/launch.sh
+++ executions/launch.sh
@@ -15,16 +15,12 @@ dataset_path=$global_path'/datasets'
 temp_files=$global_path'/executions/temp_files'
 output_folder=$global_path'/executions/aRD_workflow'
 list_path=$global_path'/lists'
-#orpha_codes=$dataset_path'/HP:0000365.txt'
-orpha_codes=$dataset_path'/HP:0000407.txt'
-#orpha_codes=$dataset_path'/hearImp_orpha_codes'
-#orpha_codes=$dataset_path'/orphas_2012.txt' #antiguo: raquel_aRD_orpha_codes.txt
-#orpha_codes='/mnt/home/users/pab_001_uma/pedro/proyectos/angio/results/orpha_codes'
+orpha_codes=$dataset_path'/HP:0000365.txt'
 export PATH=$scripts_path:$PATH
 source ~soft_bio_267/initializes/init_pets
 source ~soft_bio_267/initializes/init_python
 
-mkdir -p $temp_files $output_folder $dataset_path
+mkdir -p $output_folder $dataset_path $temp_files
 
 ontology_file=$dataset_path'/mondo.obo'
 monarch_gene_disease=$dataset_path'/gene_disease.all.tsv'
@@ -38,32 +34,44 @@ if [ "$1" == "1" ]; then
 	mv $dataset_path'/9606.protein.links.v11.5.txt' $string_network # copied from original execution
 	
 	### File with Human GeneID codes and STRING IDs (used as dictionary)
-    wget https://stringdb-static.org/download/protein.info.v11.5/9606.protein.info.v11.5.txt.gz -O $temp_files"/9606.protein.info.v11.5.txt.gz"
-    gunzip $temp_files"/9606.protein.info.v11.5.txt.gz"
+    wget https://stringdb-static.org/download/protein.info.v11.5/9606.protein.info.v11.5.txt.gz -O $dataset_path"/9606.protein.info.v11.5.txt.gz"
+    gunzip $dataset_path"/9606.protein.info.v11.5.txt.gz"
 
     ### File with HPO phenotypes
 	####wget http://purl.obolibrary.org/obo/hp/hpoa/genes_to_phenotype.txt -O $temp_files"/genes_to_phenotype.txt"
-	wget http://purl.obolibrary.org/obo/hp/hpoa/phenotype.hpoa -O $temp_files'/phenotype.hpoa'
+	wget http://purl.obolibrary.org/obo/hp/hpoa/phenotype.hpoa -O $dataset_path'/phenotype.hpoa'
 	
     ### MONDO File with genes and diseases
 	wget 'http://purl.obolibrary.org/obo/mondo.obo' -O $ontology_file
 	wget 'https://archive.monarchinitiative.org/latest/tsv/all_associations/gene_disease.all.tsv.gz' -O $monarch_gene_disease'.gz'
 	gunzip $monarch_gene_disease'.gz'
+
+	touch $temp_files/dis_white_list
+fi
+
+if [ "$1" == "1a" ]; then
+	term=$2
+	#MONDO:0003847 Mendelian disease (filter by those diseases with a genetic component)
+	echo 'Filtering list of phenotypes'
+	semtools.py -C $2 -O MONDO > $temp_files/filtered_mondo_codes.txt
+	semtools.py -i $temp_files/filtered_mondo_codes.txt --list -k "Orphanet:[0-9]*" --xref_sense -O MONDO -o $temp_files/mondo_orpha_codes.txt
+	sed 's/Orphanet/ORPHA/g' $temp_files/mondo_orpha_codes.txt | cut -f 2 > $temp_files/dis_white_list
 fi
 
 if [ "$1" == "1b" ]; then
-	#source ~soft_bio_267/initializes/init_semtools
-	echo 'preparing files'
-	grep -v '#' $temp_files/phenotype.hpoa | grep -v -w 'NOT' | cut -f 1,2,4 > $temp_files/dis_name_phen.txt
+	echo 'preparing files...'
+	intersect_columns.py -a $orpha_codes -b $temp_files/dis_white_list > $temp_files/filtered_orpha_codes.txt	
+	grep -v '#' $dataset_path/phenotype.hpoa | grep -v -w 'NOT' | cut -f 1,2,4 > $temp_files/dis_name_phen.txt
 	echo -e "DiseaseID\tHPOID" > $temp_files/disease_hpos.txt
-	#grep -w -F -f $orpha_codes $temp_files/dis_name_phen.txt | cut -f 1,3 | sort -u | aggregate_column_data.py -i - -s '|' -x 1 -a 2 >> $temp_files/disease_hpos.txt
-	grep -w -F -f $orpha_codes $temp_files/dis_name_phen.txt | cut -f 1,3 | sort -u | aggregate_column_data.rb -i - -s '|' -x 0 -a 1 >> $temp_files/disease_hpos.txt
-	sed 's/ORPHA/Orphanet/g' $orpha_codes > $temp_files/orphanet_IDs
+	grep -w -F -f $temp_files/filtered_orpha_codes.txt $temp_files/dis_name_phen.txt | cut -f 1,3 | sort -u | aggregate_column_data.rb -i - -s '|' -x 0 -a 1 >> $temp_files/disease_hpos.txt
+	sed 's/ORPHA/Orphanet/g' $temp_files/filtered_orpha_codes.txt > $temp_files/orphanet_IDs
+	# semtools with --list set is to get a dictionary
+	echo 'executing semtools...'
 	semtools.py -i $temp_files/orphanet_IDs --list -k "Orphanet:[0-9]*|OMIM:[0-9]*" -O MONDO -o $temp_files/disease_mondo_codes.txt
 	sed -i 's/Orphanet/ORPHA/g' $temp_files/disease_mondo_codes.txt
-	get_mondo_genes.py -i $temp_files/disease_mondo_codes.txt -m $monarch_gene_disease -o $temp_files/disease_mondo_genes_py.txt
+	get_mondo_genes.py -i $temp_files/disease_mondo_codes.txt -m $monarch_gene_disease -o $temp_files/disease_mondo_genes.txt
 	cut -f 1,3 $temp_files/disease_mondo_genes.txt > $temp_files/disease_genes.txt
-	standard_name_replacer.py -i $string_network -I $temp_files"/9606.protein.info.v11.5.txt" -c 1,2 -s ' ' | tr ' ' '\t' > $temp_files/string_transl_network.txt
+	standard_name_replacer.py -i $string_network -I $dataset_path"/9606.protein.info.v11.5.txt" -c 1,2 -s ' ' | tr ' ' '\t' > $temp_files/string_transl_network.txt
 fi
 
 gene_filter_values=( 0 )
@@ -72,7 +80,6 @@ similarity_measures=( 'lin' )
 #similarity_measures=( 'lin' 'resnik' 'jiang_conrath' )
 min_groups=( 0 )
 if [ "$1" == "2" ]; then
-	touch $list_path'/white_list'
 	source ~soft_bio_267/initializes/init_autoflow
 	echo 'Launching analysis'
 	for similarity_measure in "${similarity_measures[@]}"
@@ -86,20 +93,17 @@ if [ "$1" == "2" ]; then
 					execution_name=$similarity_measure"_"$min_group"_"$combined_score"_"$gene_filter_value
 					var_info=`echo -e "\\$similarity_measure=$similarity_measure,
 					\\$string_network=$temp_files/string_transl_network.txt,
-					\\$gmt=$global_path/lists/all.gmt,
-					\\$robustness_fraction=0.02,
-					\\$hub_zscore=2.5,
+					\\$hub_zscore=3,
 					\\$white_list=$list_path'/white_list',
-					\\$gene_ref_list=$list_path'/wp_angio',
-					\\$string_dict=$temp_files/9606.protein.info.v11.5.txt,
+					\\$string_dict=$dataset_path/9606.protein.info.v11.5.txt,
 					\\$combined_score=$combined_score,
 					\\$min_group=$min_group,
 					\\$gene_filter=$gene_filter_value,
 					\\$disease_gene_file=$temp_files/disease_genes.txt,
 					\\$phenotype_annotation=$temp_files'/dis_name_phen.txt',
-					\\$disease_mondo_genes=$temp_files/disease_mondo_genes.txt,
-					\\$disease_hpo_file=$temp_files/disease_hpos.txt,
-					\\$all_diseases=$orpha_codes,
+					\\$disease_mondo_genes=$temp_files'/disease_mondo_genes.txt',
+					\\$disease_hpo_file=$temp_files'/disease_hpos.txt',
+					\\$all_diseases=$temp_files'/filtered_orpha_codes.txt',
 					\\$report_template=$global_path/executions/templates/angio_report.erb,
 					\\$scripts_path=$scripts_path" | tr -d '[:space:]' `
 					AutoFlow -w templates/aRD_analysis.txt -t '7-00:00:00' -m '100gb' -c 4 -o $output_folder"/"$execution_name -n 'sr' -e -V $var_info $2
@@ -123,5 +127,9 @@ elif [ "$1" == "2b" ]; then
 			done
 		done
 	done
-
 fi
+
+#orpha_codes=$dataset_path'/HP:0000407.txt'
+#orpha_codes=$dataset_path'/hearImp_orpha_codes'
+#orpha_codes=$dataset_path'/orphas_2012.txt' #antiguo: raquel_aRD_orpha_codes.txt
+#orpha_codes='/mnt/home/users/pab_001_uma/pedro/proyectos/angio/results/orpha_codes'
