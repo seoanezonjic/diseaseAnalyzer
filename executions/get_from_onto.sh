703da8b73e20d4061e6ad9cf4076aad48aee1bbd
seoanezonjic <darklordsone@hotmail.com>
2024-03-11 21:59:36 +0100

    Change monarch api queries to pets package

--- executions/get_from_onto.sh
+++ executions/get_from_onto.sh
@@ -21,17 +21,21 @@ if [ "$1" == "1" ]; then
 		codename='OMIM'
 		codetag='OMIM'
 	fi
-	enc_term=`echo -e $term | sed 's/:/%3A/g'`
-	wget 'https://api.monarchinitiative.org/api/bioentity/phenotype/'$enc_term'/diseases?rows=10000&facet=false&unselect_evidence=false&exclude_automatic_assertions=false&fetch_objects=false&use_compact_associations=false&direct_taxon=false' -O $list_path/$term.json
-	get_items.py -i $list_path/$term.json > $list_path/$term.mondo		
+	monarch_entities -d $term -r phenotype-disease -o $list_path/$term'_disease_results.tab'
+	cut -f 1 $list_path/$term'_disease_results.tab' | sort -u > $list_path/$term.mondo
 	semtools -i $list_path/$term.mondo --list -k $codename":[0-9]*" --xref_sense -O MONDO -o $list_path/$term'.dict'
 	cut -f 2 $list_path/$term'.dict' | sort -u | sed "s/$codename/$codetag/g" > $dataset_path/$term.txt
 fi
 
 if [ "$1" == "2" ]; then
 
-	wget https://archive.monarchinitiative.org/latest/tsv/gene_associations/gene_disease.9606.tsv.gz -O $temp_files'/gene_disease.9606.tsv.gz'
+	wget https://data.monarchinitiative.org/latest/tsv/gene_associations/gene_disease.9606.tsv.gz -O $temp_files'/gene_disease.9606.tsv.gz'
 	gunzip $temp_files'/gene_disease.9606.tsv.gz'
+	# the preivous source seems deprecated. See that this source could replace iti (from https://hpo.jax.org/app/data/annotations) or if there is an alternative in the new monarch app:
+	# wget 'https://objects.githubusercontent.com/github-production-release-asset-2e65be/41063438/545054dd-747d-4bba-ab19-4f18858f39af?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240311%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240311T180105Z&X-Amz-Expires=300&X-Amz-Signature=08e1f83f4ccb9b4f91d07302eec125a1ac646e596a66cef106cba8fd3b0f4edd&X-Amz-SignedHeaders=host&actor_id=4126037&key_id=0&repo_id=41063438&response-content-disposition=attachment%3B%20filename%3Dgenes_to_disease.txt&response-content-type=application%2Foctet-stream' -O genes_to_disease.txt
+	# maybe we have to use the new api to retrieve this data (see module 1)
+
+
 	wget http://geneontology.org/gene-associations/goa_human.gaf.gz -O $temp_files'/goa_human.gaf.gz'
 	gunzip $temp_files'/goa_human.gaf.gz'
 
